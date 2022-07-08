a93e76b2ccf18aee1ef06289b424ce68da438c15
Alexandr <55240680+3od1ak@users.noreply.github.com>
2022-07-08 23:25:51 +0300

    init project

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(`Server listening on port ${port}`);
+});
