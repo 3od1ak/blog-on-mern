7e8aeb8d5ac5b8ec1b330b4e8d0bc566a0024bcf
Alexandr <fmilone@yandex.ru>
2022-07-13 20:28:36 +0300

    Изменено имя файла с auth на register

--- validations/register.js
+++ validations/register.js
@@ -1,7 +1,7 @@
 import { body } from 'express-validator';
 
 export const registerValidatior = [
-  body('email').isEmail(),
+  body('email', 'Неверный формат почты').isEmail(),
   // проверка поля на то, является ли оно почтой
   body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
   // проверка на кол-во символов в поле
