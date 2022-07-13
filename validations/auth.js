8150f508a62978125ec10fa3be91a13163f49ab5
Alexandr <fmilone@yandex.ru>
2022-07-13 09:54:27 +0300

    Мелкие изменения файла

--- validations/auth.js
+++ validations/auth.js
@@ -3,7 +3,7 @@ import { body } from 'express-validator';
 export const registerValidatior = [
   body('email').isEmail(),
   // проверка поля на то, является ли оно почтой
-  body('password').isLength({ min: 5 }),
+  body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
   // проверка на кол-во символов в поле
   body('fullName').isLength({ min: 3 }),
   body('avatarUrl')
