ac4b2674c39d32e63969ec812fa5741f029e3929
Alexandr <fmilone@yandex.ru>
2022-07-13 20:22:48 +0300

    Добавлен multer, как обработчик загружаемых данных, добавлен обработчик валидационных ошибок

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
 
 import { registerValidatior } from './validations/auth.js';
 import { loginValidatior } from './validations/login.js';
@@ -9,6 +10,7 @@ import checkAuth from './utils/checkAuth.js';
 
 import * as UserController from './controllers/UserController.js';
 import { create, getAll, getOne, remove, update } from './controllers/PostControllers.js';
+import handleValidationsErrors from './utils/handleValidationsErrors.js';
 
 const port = 3000;
 
@@ -22,25 +24,65 @@ mongoose
 const app = express();
 // создание Express-приложения. Вся логика Express хранится в этой переменной
 
+const storage = multer.diskStorage({
+  // создаем хранилище для мультера
+  destination: (_, __, cb) => {
+    // функция объясняет, какой путь нужно использовать
+    cb(null, 'uploads');
+    // функция не получает никаких ошибок
+    // сохранить загруженные файлы в папку uploads
+  },
+  // путь сохранения картинок
+
+  filename: (_, file, cb) => {
+    // как будет называться файл
+    // file - будет передаваться при отправке запроса
+    cb(null, file.originalname);
+    // file.originalname - вытащить из file оригинальное название
+  },
+});
+// хранилище для всех картинок
+
+const upload = multer({ storage });
+// где храняться файлы мультера
+
 app.use(express.json());
 //  позволяет читать json, который будет приходить в запросы
+app.use('/uploads', express.static('uploads'));
+// сказать Express, что если придет любой запрой на /uploads, с помощью статической функции проверить,
+// есть ли в этой папке то, что передается в запросе роута
 
-app.post('/auth/login', loginValidatior, UserController.login);
+// --> Роуты аккаунта
+app.post('/auth/login', loginValidatior, handleValidationsErrors, UserController.login);
+// сначала идёт валидация, если валидационные ошибки есть - парсить их и возвращать
+// если ошибок нет - только тогда идёт UserController
 
-app.post('/auth/register', registerValidatior, UserController.register);
+app.post('/auth/register', registerValidatior, handleValidationsErrors, UserController.register);
 
 app.get('/auth/me', checkAuth, UserController.getMe);
+// <-- Роуты аккаунта
 
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  // перед тем, как выполнить что-то, использовать связь upload.single
+  // и дальнейшие действия будут только если в upload.single что-то придет
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+    // путь, по которому сохранена картинка
+  });
+});
+
+// <-- Роуты постов
 app.get('/posts', getAll);
-// - получение всех статей
-app.post('/posts', checkAuth, postCreateValidatior, create);
-// - создание статьи
+// получение всех статей
 app.get('/posts/:id', getOne);
-// - получение одной статьи, :id - динамический параметп
+// получение одной статьи, :id - динамический параметп
+app.post('/posts', checkAuth, postCreateValidatior, handleValidationsErrors, create);
+// создание статьи
 app.delete('/posts/:id', checkAuth, remove);
-// - удаление статьи
-app.patch('/posts/:id', update);
-// - обновление статьи
+// удаление статьи
+app.patch('/posts/:id', checkAuth, postCreateValidatior, handleValidationsErrors, update);
+// обновление статьи
+// <-- Роуты постов
 
 app.listen(port, (err) => {
   // слушатель событий на порте 3000 (port)
